3b1f9caf7d9c3159a3933801cbeccb381909d566
Doan Minh <doanminhyb04@gmail.com>
2025-01-23 01:02:31 +0700

    spring security

--- src/main/java/vn/hoidanit/laptopshop/HelloController.java
+++ src/main/java/vn/hoidanit/laptopshop/HelloController.java
@@ -7,7 +7,16 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class HelloController {
     @GetMapping("/")
     public String index() {
-        return "Hello World from Spring Boot!";
+        return "Hello World from Spring Boot! I'm Minh";
     }
 
+    @GetMapping("/user")
+    public String getUserPage() {
+        return "this is user page";
+    }
+
+    @GetMapping("/admin")
+    public String getAdminPage() {
+        return "this is admin page";
+    }
 }
--- src/main/java/vn/hoidanit/laptopshop/LaptopshopApplication.java
+++ src/main/java/vn/hoidanit/laptopshop/LaptopshopApplication.java
@@ -2,12 +2,20 @@ package vn.hoidanit.laptopshop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class LaptopshopApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(LaptopshopApplication.class, args);
+		ApplicationContext appCon = SpringApplication.run(LaptopshopApplication.class, args);
+		int cnt = 0;
+		System.out.println("List of Beans:");
+		for (String s : appCon.getBeanDefinitionNames()) {
+			System.out.println(s);
+			++cnt;
+		}
+		System.out.println(cnt);
 	}
 
 }
